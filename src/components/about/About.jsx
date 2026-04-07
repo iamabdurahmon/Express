24cb311f398619f636a008e49d191cb17d6f08c7
FirdavsAbdullayev <firdavsabdullayev622@gmail.com>
2026-04-07 16:07:07 +0500

    About page started

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import styles from "./about.module.css"
 
 const About = () => {
-  return <div></div>;
+  return <div className={styles.Wrapper}>
+    <div className="Content">
+      <div className={styles.Title}>
+        <span className={styles.Sub}>
+          About Us
+        </span>
+
+        <h1 className={styles.Heading}>
+          Empowering Your Business with Tailored Logistics Solutions
+        </h1>
+      </div>
+    </div>
+  </div>;
 };
 
 export default About;
