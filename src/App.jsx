469b6d944fb40702ce34389f7b101dd0c8c2a919
FirdavsAbdullayev <firdavsabdullayev622@gmail.com>
2026-04-08 09:38:29 +0500

    Firdavs all done out of functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import About from "./components/about/About";
-
+import HowItWorks from "./components/HowItWorks/HowItWorks";
+import VideoBox from "./components/6 Video Box/VideoBox";
+import Services from "./components/Services/Services";
 const App = () => {
   return (
     <div>
       <About />
+      <HowItWorks />
+      <VideoBox />
+      <Services />
     </div>
   );
 };
